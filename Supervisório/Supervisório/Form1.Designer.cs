2a022a9d1616a0902700d86f561b5173fdaecebf
Tiago Morais <tiago.mdsantos@gmail.com>
2017-02-25 23:45:08 -0300

    update
    plotting 2 data from arduino

--- Supervisório/Supervisório/Form1.Designer.cs
+++ Supervisório/Supervisório/Form1.Designer.cs
@@ -28,17 +28,34 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.Windows.Forms.DataVisualization.Charting.LineAnnotation lineAnnotation7 = new System.Windows.Forms.DataVisualization.Charting.LineAnnotation();
-			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-			System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-			System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.LineAnnotation lineAnnotation1 = new System.Windows.Forms.DataVisualization.Charting.LineAnnotation();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
 			this.btn_tempAsub = new System.Windows.Forms.Button();
 			this.btn_tempAplus = new System.Windows.Forms.Button();
 			this.btn_tempBsub = new System.Windows.Forms.Button();
 			this.btn_tempBplus = new System.Windows.Forms.Button();
 			this.btn_Fechar = new System.Windows.Forms.Button();
-			this.cpuChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
-			((System.ComponentModel.ISupportInitialize)(this.cpuChart)).BeginInit();
+			this.tempChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.cb_ports = new System.Windows.Forms.ComboBox();
+			this.data_tb = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.btn_Start = new System.Windows.Forms.Button();
+			this.btn_stop = new System.Windows.Forms.Button();
+			this.btn_Save = new System.Windows.Forms.Button();
+			this.lb_TempA_sp = new System.Windows.Forms.Label();
+			this.lb_TempA_value = new System.Windows.Forms.Label();
+			this.lb_TempB_sp = new System.Windows.Forms.Label();
+			this.lb_TempB_value = new System.Windows.Forms.Label();
+			this.lb_tempA_control = new System.Windows.Forms.Label();
+			this.lb_tempB_control = new System.Windows.Forms.Label();
+			this.btn_reset = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.tempChart)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// btn_tempAsub
@@ -83,7 +100,7 @@
 			// 
 			// btn_Fechar
 			// 
-			this.btn_Fechar.Location = new System.Drawing.Point(448, 479);
+			this.btn_Fechar.Location = new System.Drawing.Point(715, 561);
 			this.btn_Fechar.Name = "btn_Fechar";
 			this.btn_Fechar.Size = new System.Drawing.Size(75, 23);
 			this.btn_Fechar.TabIndex = 4;
@@ -91,33 +108,193 @@
 			this.btn_Fechar.UseVisualStyleBackColor = true;
 			this.btn_Fechar.Click += new System.EventHandler(this.btn_Fechar_Click);
 			// 
-			// cpuChart
+			// tempChart
 			// 
-			lineAnnotation7.Name = "Series1";
-			this.cpuChart.Annotations.Add(lineAnnotation7);
-			chartArea7.Name = "ChartArea1";
-			this.cpuChart.ChartAreas.Add(chartArea7);
-			legend7.Name = "Legend1";
-			this.cpuChart.Legends.Add(legend7);
-			this.cpuChart.Location = new System.Drawing.Point(45, 24);
-			this.cpuChart.Name = "cpuChart";
-			this.cpuChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
-			series7.ChartArea = "ChartArea1";
-			series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-			series7.Legend = "Legend1";
-			series7.Name = "Series1";
-			this.cpuChart.Series.Add(series7);
-			this.cpuChart.Size = new System.Drawing.Size(442, 359);
-			this.cpuChart.TabIndex = 5;
-			this.cpuChart.Text = "chart1";
+			lineAnnotation1.Name = "Series1";
+			this.tempChart.Annotations.Add(lineAnnotation1);
+			chartArea1.Name = "ChartArea1";
+			this.tempChart.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.tempChart.Legends.Add(legend1);
+			this.tempChart.Location = new System.Drawing.Point(12, 12);
+			this.tempChart.Name = "tempChart";
+			this.tempChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+			series1.BorderWidth = 2;
+			series1.ChartArea = "ChartArea1";
+			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series1.Color = System.Drawing.Color.IndianRed;
+			series1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			series1.Legend = "Legend1";
+			series1.MarkerSize = 10;
+			series1.Name = "tempA_SetPoint";
+			series2.BorderWidth = 2;
+			series2.ChartArea = "ChartArea1";
+			series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series2.Color = System.Drawing.Color.Aqua;
+			series2.Legend = "Legend1";
+			series2.Name = "tempB_SetPoint";
+			series3.BorderWidth = 3;
+			series3.ChartArea = "ChartArea1";
+			series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series3.Color = System.Drawing.Color.Red;
+			series3.Legend = "Legend1";
+			series3.Name = "tempA_usb";
+			series4.BorderWidth = 3;
+			series4.ChartArea = "ChartArea1";
+			series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series4.Color = System.Drawing.Color.Blue;
+			series4.Legend = "Legend1";
+			series4.Name = "tempB_usb";
+			this.tempChart.Series.Add(series1);
+			this.tempChart.Series.Add(series2);
+			this.tempChart.Series.Add(series3);
+			this.tempChart.Series.Add(series4);
+			this.tempChart.Size = new System.Drawing.Size(778, 354);
+			this.tempChart.TabIndex = 5;
+			this.tempChart.Text = "chart1";
+			title1.Name = "Controle de temperatura";
+			this.tempChart.Titles.Add(title1);
+			// 
+			// cb_ports
+			// 
+			this.cb_ports.FormattingEnabled = true;
+			this.cb_ports.Location = new System.Drawing.Point(669, 422);
+			this.cb_ports.Name = "cb_ports";
+			this.cb_ports.Size = new System.Drawing.Size(121, 21);
+			this.cb_ports.TabIndex = 6;
+			this.cb_ports.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// data_tb
+			// 
+			this.data_tb.Location = new System.Drawing.Point(45, 461);
+			this.data_tb.Multiline = true;
+			this.data_tb.Name = "data_tb";
+			this.data_tb.Size = new System.Drawing.Size(442, 124);
+			this.data_tb.TabIndex = 7;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(628, 425);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "Porta:";
+			// 
+			// btn_Start
+			// 
+			this.btn_Start.Location = new System.Drawing.Point(715, 449);
+			this.btn_Start.Name = "btn_Start";
+			this.btn_Start.Size = new System.Drawing.Size(75, 23);
+			this.btn_Start.TabIndex = 9;
+			this.btn_Start.Text = "Start";
+			this.btn_Start.UseVisualStyleBackColor = true;
+			this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+			// 
+			// btn_stop
+			// 
+			this.btn_stop.Location = new System.Drawing.Point(715, 478);
+			this.btn_stop.Name = "btn_stop";
+			this.btn_stop.Size = new System.Drawing.Size(75, 23);
+			this.btn_stop.TabIndex = 10;
+			this.btn_stop.Text = "Stop";
+			this.btn_stop.UseVisualStyleBackColor = true;
+			this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+			// 
+			// btn_Save
+			// 
+			this.btn_Save.Location = new System.Drawing.Point(715, 508);
+			this.btn_Save.Name = "btn_Save";
+			this.btn_Save.Size = new System.Drawing.Size(75, 23);
+			this.btn_Save.TabIndex = 11;
+			this.btn_Save.Text = "Save";
+			this.btn_Save.UseVisualStyleBackColor = true;
+			this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+			// 
+			// lb_TempA_sp
+			// 
+			this.lb_TempA_sp.AutoSize = true;
+			this.lb_TempA_sp.Location = new System.Drawing.Point(631, 129);
+			this.lb_TempA_sp.Name = "lb_TempA_sp";
+			this.lb_TempA_sp.Size = new System.Drawing.Size(60, 13);
+			this.lb_TempA_sp.TabIndex = 12;
+			this.lb_TempA_sp.Text = "Set Point A";
+			// 
+			// lb_TempA_value
+			// 
+			this.lb_TempA_value.AutoSize = true;
+			this.lb_TempA_value.Location = new System.Drawing.Point(697, 129);
+			this.lb_TempA_value.Name = "lb_TempA_value";
+			this.lb_TempA_value.Size = new System.Drawing.Size(35, 13);
+			this.lb_TempA_value.TabIndex = 13;
+			this.lb_TempA_value.Text = "label2";
+			// 
+			// lb_TempB_sp
+			// 
+			this.lb_TempB_sp.AutoSize = true;
+			this.lb_TempB_sp.Location = new System.Drawing.Point(631, 158);
+			this.lb_TempB_sp.Name = "lb_TempB_sp";
+			this.lb_TempB_sp.Size = new System.Drawing.Size(60, 13);
+			this.lb_TempB_sp.TabIndex = 14;
+			this.lb_TempB_sp.Text = "Set Point B";
+			// 
+			// lb_TempB_value
+			// 
+			this.lb_TempB_value.AutoSize = true;
+			this.lb_TempB_value.Location = new System.Drawing.Point(697, 158);
+			this.lb_TempB_value.Name = "lb_TempB_value";
+			this.lb_TempB_value.Size = new System.Drawing.Size(35, 13);
+			this.lb_TempB_value.TabIndex = 15;
+			this.lb_TempB_value.Text = "label3";
+			// 
+			// lb_tempA_control
+			// 
+			this.lb_tempA_control.AutoSize = true;
+			this.lb_tempA_control.Location = new System.Drawing.Point(74, 391);
+			this.lb_tempA_control.Name = "lb_tempA_control";
+			this.lb_tempA_control.Size = new System.Drawing.Size(117, 13);
+			this.lb_tempA_control.TabIndex = 16;
+			this.lb_tempA_control.Text = "Controle do Set Point A";
+			// 
+			// lb_tempB_control
+			// 
+			this.lb_tempB_control.AutoSize = true;
+			this.lb_tempB_control.Location = new System.Drawing.Point(344, 391);
+			this.lb_tempB_control.Name = "lb_tempB_control";
+			this.lb_tempB_control.Size = new System.Drawing.Size(117, 13);
+			this.lb_tempB_control.TabIndex = 17;
+			this.lb_tempB_control.Text = "Controle do Set Point B";
+			// 
+			// btn_reset
+			// 
+			this.btn_reset.Location = new System.Drawing.Point(631, 449);
+			this.btn_reset.Name = "btn_reset";
+			this.btn_reset.Size = new System.Drawing.Size(75, 23);
+			this.btn_reset.TabIndex = 18;
+			this.btn_reset.Text = "Reset";
+			this.btn_reset.UseVisualStyleBackColor = true;
+			this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.White;
-			this.ClientSize = new System.Drawing.Size(539, 521);
-			this.Controls.Add(this.cpuChart);
+			this.ClientSize = new System.Drawing.Size(802, 597);
+			this.Controls.Add(this.btn_reset);
+			this.Controls.Add(this.lb_tempB_control);
+			this.Controls.Add(this.lb_tempA_control);
+			this.Controls.Add(this.lb_TempB_value);
+			this.Controls.Add(this.lb_TempB_sp);
+			this.Controls.Add(this.lb_TempA_value);
+			this.Controls.Add(this.lb_TempA_sp);
+			this.Controls.Add(this.btn_Save);
+			this.Controls.Add(this.btn_stop);
+			this.Controls.Add(this.btn_Start);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.data_tb);
+			this.Controls.Add(this.cb_ports);
+			this.Controls.Add(this.tempChart);
 			this.Controls.Add(this.btn_Fechar);
 			this.Controls.Add(this.btn_tempBsub);
 			this.Controls.Add(this.btn_tempBplus);
@@ -125,8 +302,9 @@
 			this.Controls.Add(this.btn_tempAsub);
 			this.Name = "Form1";
 			this.Text = "Form1";
-			((System.ComponentModel.ISupportInitialize)(this.cpuChart)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.tempChart)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -137,7 +315,20 @@
 		private System.Windows.Forms.Button btn_tempBsub;
 		private System.Windows.Forms.Button btn_tempBplus;
 		private System.Windows.Forms.Button btn_Fechar;
-		private System.Windows.Forms.DataVisualization.Charting.Chart cpuChart;
+		private System.Windows.Forms.DataVisualization.Charting.Chart tempChart;
+		private System.Windows.Forms.ComboBox cb_ports;
+		private System.Windows.Forms.TextBox data_tb;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btn_Start;
+		private System.Windows.Forms.Button btn_stop;
+		private System.Windows.Forms.Button btn_Save;
+		private System.Windows.Forms.Label lb_TempA_sp;
+		private System.Windows.Forms.Label lb_TempA_value;
+		private System.Windows.Forms.Label lb_TempB_sp;
+		private System.Windows.Forms.Label lb_TempB_value;
+		private System.Windows.Forms.Label lb_tempA_control;
+		private System.Windows.Forms.Label lb_tempB_control;
+		private System.Windows.Forms.Button btn_reset;
 	}
 }
 
--- Supervisório/Supervisório/Form1.cs
+++ Supervisório/Supervisório/Form1.cs
@@ -7,84 +7,220 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
-using System.Threading;
-using System.Diagnostics;
+
+
+
 namespace Supervisório
 {
 	public partial class Form1 : Form
 	{
+		
+		private double tempA = 70;
+		private double tempB = 5;
+		
 
-		private Thread cpuThread;
-		private double[] cpuArray = new double[60];
+		private SerialPort temp = new SerialPort();
+		private DateTime date;
+		private String in_data = "";
+
+		string[] ports = SerialPort.GetPortNames();
+
+
 
 		public Form1()
 		{
 			InitializeComponent();
 
-			cpuThread = new Thread(new ThreadStart(this.getperfomancecounters));
-			cpuThread.IsBackground = true;
+			lb_TempA_value.Text = tempA + " °C";
+			lb_TempB_value.Text = tempB + " °C";
 
-			cpuThread.Start();
-
-		}
-
-		private void getperfomancecounters()
-		{
-			var cpuPerfCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
-
-			while (true)
+			foreach (string port in ports)	//checking which usb ports are available
 			{
-				cpuArray[cpuArray.Length - 1] = Math.Round(cpuPerfCounter.NextValue(),0);
-				Array.Copy(cpuArray,1,cpuArray, 0, cpuArray.Length - 1);
-				if (cpuChart.IsHandleCreated)
-				{
-					this.Invoke((MethodInvoker)delegate { UpdateCpuChart(); });
-				}
-				else
-				{
-					//.....
-				}
-				Thread.Sleep(200);
+				cb_ports.Items.Add(port);
 			}
-		}
 
-		private void UpdateCpuChart()
-		{
-			cpuChart.Series["Series1"].Points.Clear();
-			for(int i = 0; i < cpuArray.Length - 1; ++i)
-			{
-				cpuChart.Series["Series1"].Points.AddY(cpuArray[i]);
-			}
+
 		}
 
 
-
-
+		
 		private void btn_Fechar_Click(object sender, EventArgs e)
 		{
 
-			
+			Application.Exit();
 		}
 
 		private void btn_tempAsub_Click(object sender, EventArgs e)
 		{
-
+			tempA += -1;
+			lb_TempA_value.Text = tempA + " °C";
 		}
 
 		private void btn_tempAplus_Click(object sender, EventArgs e)
 		{
-
+			tempA += 1;
+			lb_TempA_value.Text = tempA + " °C";
 		}
 
 		private void btn_tempBsub_Click(object sender, EventArgs e)
 		{
-
+			tempB += -1;
+			lb_TempB_value.Text = tempB + " °C";
 		}
 
 		private void btn_tempBplus_Click(object sender, EventArgs e)
 		{
+			tempB += 1;
+			lb_TempB_value.Text = tempB + " °C";
+		}
 
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btn_Start_Click(object sender, EventArgs e)
+		{
+
+
+			temp.Close();
+			temp.BaudRate = 9600;
+			try
+			{
+				temp.PortName = cb_ports.Items[this.cb_ports.SelectedIndex].ToString();
+			}
+			catch (Exception)
+			{
+
+			}
+			temp.Parity = Parity.None;
+			temp.DataBits = 8;
+			temp.StopBits = StopBits.One;
+			temp.DataReceived += temp_dataReceived;
+			
+			try
+			{
+				temp.Open();
+				data_tb.Text = "";
+				temp.WriteLine("on");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Error");
+
+			}
+		}
+
+		private void btn_stop_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				temp.WriteLine("off");
+				temp.Close();
+			}
+			catch(Exception ex2)
+			{
+				MessageBox.Show(ex2.Message, "Error");
+			}
+
+		}
+
+
+
+		void temp_dataReceived(object sender, SerialDataReceivedEventArgs e)
+		{
+			
+
+			this.Invoke(new EventHandler(displaydata_event));
+			try
+			{
+				in_data = temp.ReadLine();
+			}
+			catch(Exception) { }
+		}
+
+
+		int num = 0;
+		double time = 0;
+		private void displaydata_event(object sender, EventArgs e)
+		{
+			time += 0.5;
+			tempChart.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+			tempChart.ChartAreas[0].RecalculateAxesScale();
+
+			
+			tempChart.Series["tempA_SetPoint"].Points.AddXY(time,tempA);
+			tempChart.Series["tempB_SetPoint"].Points.AddXY(time,tempB);
+
+			try
+			{
+				double temp_sensorA;
+				double temp_sensorB;
+
+				string controler_read = in_data;
+				int dotcomma_position = 0;
+
+				if (controler_read.Contains(";"))
+				{
+					dotcomma_position = controler_read.IndexOf(";");
+					temp_sensorA = Convert.ToDouble(controler_read.Substring(0, dotcomma_position))/100;
+					int Length_aux = controler_read.Length - dotcomma_position -1;
+					temp_sensorB = Convert.ToDouble(controler_read.Substring(dotcomma_position + 1, Length_aux)) / 100; ;
+
+
+					tempChart.Series["tempA_usb"].Points.AddXY(time, temp_sensorA);
+					tempChart.Series["tempB_usb"].Points.AddXY(time, temp_sensorB);
+					num++;
+					data_tb.AppendText("Temperatura A: "+temp_sensorA+"\t\t"+"Temperatura B: "+temp_sensorB + "\n");
+				}
+				else { }
+			}
+			catch (FormatException) { }
+
+			if (num >= 100)
+			{
+
+				tempChart.Series["tempA_SetPoint"].Points.RemoveAt(0);
+				tempChart.Series["tempB_SetPoint"].Points.RemoveAt(0);
+				tempChart.Series["tempA_usb"].Points.RemoveAt(0);
+				tempChart.Series["tempB_usb"].Points.RemoveAt(0);
+
+			}
+
+		}
+
+
+
+		private void btn_Save_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				date = DateTime.Now;
+				string pathfile = @"C:\Users\tiago\Dropbox\PI - Supervisório\PC\dados salvos\";
+				string fileName = date.Day.ToString() + "." + date.Month.ToString() + "." + date.Year.ToString()
+					+ "-" + date.Hour.ToString() + "." + date.Minute.ToString() + ".txt";
+				System.IO.File.WriteAllText(pathfile + fileName, data_tb.Text);
+				MessageBox.Show("Data has been saved to " + pathfile, "Save File");
+			}
+			catch(Exception ex3)
+			{
+				MessageBox.Show(ex3.Message, "Error");
+			}
+		}
+
+		private void btn_reset_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				tempChart.Series[0].Points.Clear();
+				tempChart.Series[1].Points.Clear();
+				tempChart.Series[2].Points.Clear();
+				tempChart.Series[3].Points.Clear();
+				data_tb.Text = "";
+			}
+			catch (Exception) { }
 		}
 	}
 }
